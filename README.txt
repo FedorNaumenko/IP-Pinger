c0e78e0fe21cf2bcc1a6a27aedfddd6c9c5fafd8
Fedor Naumenko <113431511+FedorNaumenko@users.noreply.github.com>
2023-12-28 16:16:59 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,18 +1,72 @@
-# PingerApp
+# IP Pinger
 
-PingerApp is a Python application that allows you to ping multiple IP addresses from a list.
+IP Pinger is a Python application that allows users to monitor multiple IP addresses. It sends ping requests to each IP address and displays their response times and TTL (Time To Live) values. The application is built with a Tkinter GUI, providing a user-friendly interface for managing IP addresses.
 
 ## Features
 
-- Load IP addresses from a text file
-- Ping multiple IP addresses concurrently
-- Display the ping results in a table
-- Remember the last opened file path
-- Sort the table by IP address or name
+- Add and monitor multiple IP addresses.
+- Display the ping response time and TTL for each IP address.
+- Interactive GUI with options to increase/decrease font size.
+- Load IP addresses from a text file.
+- Save the last opened file path for quick access.
+- Real-time updates of IP status.
+- Context menu for easy access to common actions.
 
 ## Usage
 
-1. Run `main.py` to start the application.
-2. Click on the 'Load IPs' button to load IP addresses from a text file.
-3. Click on the 'Start Pinging' button to start pinging the loaded IP addresses.
-4. The ping results will be displayed in the table.
+To run the script, use a Python interpreter as shown below:
+
+```bash
+python pinger_app.py
+
+### Adding an IP Address
+
+1. Use the "Add IP" option in the Options menu.
+2. Enter the Name and IP Address in the new window.
+3. Click "Add IP" to add the IP to the monitoring list.
+
+### Editing an IP Address
+
+1. Double-click on an IP in the list.
+2. Modify the Name or IP Address.
+
+### Removing an IP Address
+
+1. Right-click on an IP and select "Remove IP".
+
+### Pinging an IP Address
+
+1. Right-click on an IP and select "Ping This IP" to start monitoring the ping response.
+2. Right-click and select "Stop Pinging This IP" to stop monitoring.
+
+### Loading IPs from a File
+
+1. Use the "Choose file" option in the Options menu.
+2. Select a .txt file with IP addresses.
+
+### Saving the Last Opened File Path
+
+- The application automatically saves the path of the last opened file.
+
+### Increasing/Decreasing Font Size
+
+- Use the "+" and "-" options in the menu bar to adjust the font size.
+
+## Dependencies
+
+- Python 3.7 or later.
+- Tkinter (usually comes with Python).
+- Additional libraries: os, platform, subprocess, re, threading, asyncio, winreg.
+
+## Limitations
+
+- Currently, the application is tailored for Windows environments, but it can be modified for other operating systems.
+- The application does not support IPv6 addresses yet.
+
+## Contributing
+
+Contributions to improve IP Pinger are welcome. Please feel free to fork the repository and submit pull requests.
+
+## License
+
+IP Pinger is released under the MIT License.
--- main.py
+++ main.py
@@ -7,15 +7,21 @@ import re
 import threading
 from tkinter import filedialog
 import winreg
+import tkinter.simpledialog as simpledialog
+import asyncio
 
-class PingerApp:
+class PingerApp():
     def __init__(self, root):
         self.root = root
         self.root.title("IP Pinger")
         self.root.configure(bg="lightgrey")  # Set the background color of the window to light grey
         self.root.geometry("450x500")  # Set the size of the window
         self.pinging_ips = set()
-
+        # Create a style
+        self.style = ttk.Style()
+        self.style.configure("Treeview", font=("TkDefaultFont", 12))
+        self.font_size = 12
+        
         # Create a frame to hold the treeview
         self.tree_frame = tk.Frame(root, bg="lightgrey")
         self.tree_frame.pack(fill=tk.BOTH, expand=True)  # Make the frame expandable
@@ -43,30 +49,14 @@ class PingerApp:
 
         self.tree.pack(fill=tk.BOTH, expand=True)  # Make the treeview expandable
         
-        # Create a menu bar
-        menu_bar = tk.Menu(root)
-
-        # Create a menu item
-        options_menu = tk.Menu(menu_bar, tearoff=0)
-
-        # Add commands to the menu item
-        options_menu.add_command(label="Add IP", command=self.add_ip)
-        options_menu.add_command(label="Choose file", command=self.load_ips_from_file)
-
-        # Add the menu item to the menu bar
-        menu_bar.add_cascade(label="Options", menu=options_menu)
-
-        # Set the menu bar
-        root.config(menu=menu_bar)
-
         # Create a right-click context menu
         context_menu = tk.Menu(root, tearoff=0)
 
         # Add commands to the context menu
-        context_menu.add_command(label="Remove IP", command=self.remove_ip)
         context_menu.add_command(label="Ping This IP", command=self.ping_selected_ip)
         context_menu.add_command(label="Stop Pinging This IP", command=self.stop_pinging_selected_ip)
-        context_menu.add_command(label="Connect To This IP", command=self.connect_to_selected_ip)
+        context_menu.add_command(label="Edit Name", command=self.edit_name)
+        context_menu.add_command(label="Remove IP", command=self.remove_ip)
 
         def show_context_menu(event):
             # Show the context menu at the position of the mouse cursor
@@ -86,6 +76,50 @@ class PingerApp:
         if self.last_opened_file_path and os.path.exists(self.last_opened_file_path):
             self.load_ips_from_file(self.last_opened_file_path)
 
+        # Bind the double click event to the edit_name function
+        self.tree.bind('<Double-1>', self.connect_to_selected_ip)
+
+        # Create a menu bar
+        self.create_menu_bar()
+
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def do_nothing(self):
+        pass
+
+    def increase_font_size(self):
+        self.font_size += 1
+        self.style.configure("Treeview", font=("TkDefaultFont", self.font_size))
+        self.create_menu_bar()  # Call create_menu_bar to update the menu bar
+            
+    def decrease_font_size(self):
+        if self.font_size > 1:
+            self.font_size -= 1
+        self.style.configure("Treeview", font=("TkDefaultFont", self.font_size))
+        self.create_menu_bar()  # Call create_menu_bar to update the menu bar
+
+    def create_menu_bar(self):
+         # Create a menu bar
+        self.menu_bar = tk.Menu(self.root)
+
+        # Create a menu item
+        options_menu = tk.Menu(self.menu_bar, tearoff=0)
+
+        # Add commands to the menu item
+        options_menu.add_command(label="Add IP", command=self.add_ip)
+        options_menu.add_command(label="Choose file", command=self.load_ips_from_file)
+
+        # Add the menu item to the menu bar
+        self.menu_bar.add_cascade(label="Options", menu=options_menu)
+
+        # Set the menu bar
+        self.root.config(menu=self.menu_bar)
+
+        self.menu_bar.add_command(label="+", command=self.increase_font_size)
+        self.menu_bar.add_command(label="Font Size: {}".format(self.font_size), command=self.do_nothing)
+        self.menu_bar.add_command(label="-", command=self.decrease_font_size)
+
     def load_ips_from_file(self, file_path=None):
          # If a file path was not provided
         if file_path is None:
@@ -102,14 +136,13 @@ class PingerApp:
                 winreg.SetValueEx(key, 'LastOpenedFilePath', 0, winreg.REG_SZ, file_path)
 
             # Open the file and load the names and IPs
-            with open(file_path, 'r', encoding='utf-8') as file:
+            with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
                 lines = file.readlines()
                 self.ip_addresses = []
                 for i in range(0, len(lines), 2):
                     name = lines[i].strip()
                     ip = lines[i + 1].strip() if i + 1 < len(lines) else ''
                     self.ip_addresses.append((name, ip))
-                    #print(f"Name: {name}, IP: {ip}")  # Debugging line
 
             # Clear the treeview
             for item in self.tree.get_children():
@@ -118,8 +151,9 @@ class PingerApp:
             # Add the names and IPs to the treeview
             for name, ip in self.ip_addresses:
                 item = self.tree.insert('', 'end', values=(name, ip, '', ''))
-                self.pinging_ips.add(ip)
-                self.ping_ip_in_background(ip, item)
+                self.pinging_ips.add(ip)  # Add the IP address to the set of pinging IPs
+                # Run the pinging function in a separate thread
+                threading.Thread(target=asyncio.run, args=(self.ping_ip_in_background(ip, item),)).start()
 
     def add_ip(self):
         # Create a new window
@@ -147,84 +181,118 @@ class PingerApp:
         self.tree.insert('', 'end', values=(name, ip, '', ''))
 
         # Write the IP to a file
-        with open(self.last_opened_file_path, 'a', encoding='utf-8') as f:
-            f.write(f'{name}\n{ip}\n')
-
+        with open(self.last_opened_file_path, 'a', encoding='utf-8', errors='ignore') as f:
+            f.write(f'\n{name}\n{ip}')
+            
         # Close the window
         window.destroy()
 
     def remove_ip(self):
         selected_item = self.tree.selection()[0]
+        ip = str(self.tree.item(selected_item)['values'][1])  # Get the IP of the selected item
+        name = str(self.tree.item(selected_item)['values'][0])  # Get the name of the selected item
+
+        # Open the file and read all lines
+        with open(self.last_opened_file_path, 'r', encoding='utf-8', errors='ignore') as file:
+            lines = file.readlines()
+
+        # Remove the lines containing the IP and name
+        lines = [line for line in lines if ip not in line and name not in line]
+       
+        # Write the remaining lines back to the file
+        with open(self.last_opened_file_path, 'w', encoding='utf-8', errors='ignore') as file:
+            for line in lines:
+                file.write(line)
+
         self.tree.delete(selected_item)
 
-    def ping_ip(self, ip):
+    async def ping_ip(self, ip):
         param = '-n' if platform.system().lower()=='windows' else '-c'
         command = ['ping', param, '1', ip]
+        process = None
+
+        # Setup for Windows to prevent opening a new window
+        if platform.system().lower() == 'windows':
+            si = subprocess.STARTUPINFO()
+            si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            si.wShowWindow = subprocess.SW_HIDE
+        else:
+            si = None
+
         try:
-            output = subprocess.check_output(command, timeout=1, creationflags=subprocess.CREATE_NO_WINDOW).decode()
-            time_match = re.search(r"Average = (\d+ms)", output)
-            ttl_match = re.search(r"TTL=(\d+)", output)
-            time = time_match.group(1) if time_match else ''
-            ttl = ttl_match.group(1) if ttl_match else ''
-            return True, time, ttl
-        except subprocess.CalledProcessError:
-            print(f"Failed to execute the 'ping' command for {ip}.")
-        except subprocess.TimeoutExpired:
-            print(f"Timeout expired while pinging {ip}.")
+            process = await asyncio.create_subprocess_exec(*command, stdout=asyncio.subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW, startupinfo=si)
+            stdout, _ = await process.communicate()
+            if stdout:
+                output = stdout.decode()
+                time_match = re.search(r"Average = (\d+ms)", output)
+                ttl_match = re.search(r"TTL=(\d+)", output)
+                time = time_match.group(1) if time_match else ''
+                ttl = ttl_match.group(1) if ttl_match else ''
+                if output.find('Request timed out') != -1:
+                    return False, '', ''
+                return True, time, ttl
+            else:
+                return False, '', ''
         except Exception as e:
             print(f"An unexpected error occurred while pinging {ip}: {e}")
-        return False, '', ''
+            return False, '', ''
 
-    def ping_selected_ip(self):
-        # Get the selected IP address
-        selected_item = self.tree.selection()[0]
-        ip = self.tree.item(selected_item)['values'][0]
-
-        # Start a new thread that runs the ping operation
-        threading.Thread(target=self.ping_ip_in_background, args=(ip, selected_item)).start()
-
-    def ping_ip_in_background(self, ip, item):
-        # Define a target function to be run in a new thread
-        def target():
+    async def ping_ip_in_background(self, ip, item):
+        while True:
             try:
-                # Try to ping the IP address
-                result, time, ttl = self.ping_ip(ip)
+                result, time, ttl = await self.ping_ip(ip)
             except Exception as e:
-                # If an error occurs, set the result, time, and ttl to default values
                 result, time, ttl = False, '', ''
-            # If the IP address is not in the list of pinging IPs or the item no longer exists, return
-            if ip not in self.pinging_ips or not self.tree.exists(item):
+
+            if ip not in self.pinging_ips: #or not self.tree.exists(item):
                 return
-            # If the ping was successful, tag the tree item with 'ping_success' and set its background to green
+            values = self.tree.item(item)['values']
+            while len(values) < 4:
+                values.append(None)
+            values[2] = time
+            values[3] = ttl
+            self.tree.item(item, values=values)
             if result:
                 self.tree.item(item, tags=('ping_success',))
                 self.tree.tag_configure('ping_success', background='green3')
-            # If the ping was not successful, tag the tree item with 'ping_fail' and set its background to red
-            else:
+            elif self.tree.item(item)['tags'] == ('ping_stop',):
+                self.tree.item(item, tags=('ping_stop',))
+                self.tree.tag_configure('ping_stop', background='white')
+            elif not result and 'ping_stop' not in self.tree.item(item)['tags']:
                 self.tree.item(item, tags=('ping_fail',))
                 self.tree.tag_configure('ping_fail', background='firebrick1')
-            # Get the current values of the tree item
-            values = self.tree.item(item)['values']
-            # Update the name, time, and ttl of the tree item
-            values[2] = time
-            values[3] = ttl
-            # Update the tree item with the new values
-            self.tree.item(item, values=values)
-            # Schedule the target function to be run again after 1 second
-            self.root.after(5000, target)
-        # Start a new thread that runs the target function
-        threading.Thread(target=target).start()
+        
+            await asyncio.sleep(3)
+        
+    def ping_selected_ip(self):
+        # Get the selected IP address
+        selected_item = self.tree.selection()[0]
+        ip = self.tree.item(selected_item)['values'][1]
+        
+        # Add the IP address to the set of pinging IPs
+        self.pinging_ips.add(ip)
+        print(f"Added {ip} to pinging IPs: {self.pinging_ips}")
+
+        self.tree.item(selected_item, tags=('ping',))
+
+        # Configure the 'ping' tag to have a green background
+        self.tree.tag_configure('ping', background='green3')
+
+        # Run the ping operation
+        #asyncio.run_coroutine_threadsafe(self.ping_ip_in_background(ip, selected_item), self.loop)
 
     def stop_pinging_selected_ip(self):
         # Get the first selected item in the treeview
         selected_item = self.tree.selection()[0]
         
         # Get the IP address of the selected item
-        ip = self.tree.item(selected_item)['values'][0]
-        
+        ip = self.tree.item(selected_item)['values'][1]
+        name = self.tree.item(selected_item)['values'][0]
+
         # Remove the IP address from the set of pinging IPs
         self.pinging_ips.discard(ip)
-        
+        print(f"Removed {ip} from pinging IPs: {self.pinging_ips}")
+
         # Tag the selected item with 'ping_stop'
         self.tree.item(selected_item, tags=('ping_stop',))
         
@@ -232,9 +300,9 @@ class PingerApp:
         self.tree.tag_configure('ping_stop', background='white')
         
         # Update the selected item's values to show that it's no longer being pinged
-        self.tree.item(selected_item, values=(ip, '', ''))
-
-    def connect_to_selected_ip(self):
+        self.tree.item(selected_item, values=(name, ip, '', ''))
+        
+    def connect_to_selected_ip(self, event=None):
         # Get the first selected item in the treeview
         selected_item = self.tree.selection()[0]
         
@@ -258,11 +326,55 @@ class PingerApp:
         # reverse sort next time
         self.tree.heading(col, command=lambda: self.sort_column(col, not reverse))
 
-# Create a root window
-root = tk.Tk()
+    def edit_name(self):
+        # Get the selected item
+        selected_item = self.tree.selection()[0]
 
-# Create an instance of MyApp
-app = PingerApp(root)
+        # Get the current name
+        current_name = self.tree.item(selected_item)['values'][0]
 
-# Start the main loop
-root.mainloop()
+        # Open a dialog to enter the new name
+        new_name = simpledialog.askstring("Input", "Enter new name",
+                                        initialvalue=current_name)
+
+        # If a new name was entered, update the name
+        if new_name is not None:
+            old_name = self.tree.item(selected_item)['values'][0]
+            self.tree.item(selected_item, values=(new_name, self.tree.item(selected_item)['values'][1]))
+            
+            # Replace the name in the file
+            self.replace_name_in_file(old_name, new_name, self.last_opened_file_path)
+            
+    def replace_name_in_file(self, old_name, new_name, filename):
+        # Open the file and read all lines
+        with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
+            lines = file.readlines()
+
+        # Replace the name in the appropriate line
+        for i, line in enumerate(lines):
+            parts = line.split()  # Split the line into parts
+            for j, part in enumerate(parts):
+                if part == old_name:  # Only replace the part that exactly matches the old name
+                    parts[j] = new_name
+            lines[i] = ' '.join(parts)  # Join the parts back together
+
+        # Write all lines back to the file
+        with open(filename, 'w', encoding='utf-8', errors='ignore') as file:
+            for i, line in enumerate(lines):
+                if i < len(lines) - 1:
+                    file.write(line + '\n')
+                else:
+                    file.write(line)
+
+def main():
+    # Create a root window
+    root = tk.Tk()
+
+    # Create an instance of MyApp
+    app = PingerApp(root)
+
+    # Start the main loop
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
